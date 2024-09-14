b2f5981506ec1545e1da1ca23518cb946443947a
Anshulnand <nandanwaranshul03@gmail.com>
2024-09-14 15:00:15 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import profilePic2 from "./images/pic6.png";
 import projectimg1 from "./images-project/windows.jpg";
 import projectimg2 from "./images-project/MUSIC PLAYER.png";
 import projectimg3 from "./images-project/CALC.png";
-import "./index.css";
+import "./index1.css";
 
 const App = () => {
 
