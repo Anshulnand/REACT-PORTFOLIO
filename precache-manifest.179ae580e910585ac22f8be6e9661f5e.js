bb09313589e4bdcd3449f4ad9ea14ac68594067b
Anshulnand <nandanwaranshul03@gmail.com>
2024-09-14 14:53:07 +0530

    Updates

---
+++ precache-manifest.179ae580e910585ac22f8be6e9661f5e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5ec649129da882c15256db69794bc4",
+    "url": "/REACT-PORTFOLIO/index.html"
+  },
+  {
+    "revision": "2fc6fdfd8eeb008d0215",
+    "url": "/REACT-PORTFOLIO/static/css/main.bc0536c0.chunk.css"
+  },
+  {
+    "revision": "4355660ae062ed8c17a1",
+    "url": "/REACT-PORTFOLIO/static/js/2.10ad2554.chunk.js"
+  },
+  {
+    "revision": "2fc6fdfd8eeb008d0215",
+    "url": "/REACT-PORTFOLIO/static/js/main.bf80489c.chunk.js"
+  },
+  {
+    "revision": "4f43bcfe9f2e80183931",
+    "url": "/REACT-PORTFOLIO/static/js/runtime~main.6ae3c69b.js"
+  },
+  {
+    "revision": "11cb846c9d8a4f10bc625a34d8dd2bf7",
+    "url": "/REACT-PORTFOLIO/static/media/CALC.11cb846c.png"
+  },
+  {
+    "revision": "608185ba69cca6f66a47e3793a1b2ec4",
+    "url": "/REACT-PORTFOLIO/static/media/MUSIC PLAYER.608185ba.png"
+  },
+  {
+    "revision": "f47c7f2b362b9b8f4d4011ee01ebddaf",
+    "url": "/REACT-PORTFOLIO/static/media/pic5.f47c7f2b.jpg"
+  },
+  {
+    "revision": "e4d042e235daf01a3c9dc61cdc8a242b",
+    "url": "/REACT-PORTFOLIO/static/media/pic6.e4d042e2.png"
+  },
+  {
+    "revision": "392468f6f60d2b705021e9ea4988c06f",
+    "url": "/REACT-PORTFOLIO/static/media/windows.392468f6.jpg"
+  }
+]);
