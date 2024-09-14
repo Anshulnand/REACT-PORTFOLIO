5186b689f6d53ed345d40c931453df69b0835bb3
Anshulnand <nandanwaranshul03@gmail.com>
2024-09-14 15:13:53 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Import Tailwind CSS file
+import './index1.css'; // Import Tailwind CSS file
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
